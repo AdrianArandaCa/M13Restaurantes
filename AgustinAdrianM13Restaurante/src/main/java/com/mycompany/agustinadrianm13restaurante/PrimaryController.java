8491696ac85f687fc0411892b65e10e139ff90a8
Agustin <agusti_cintassoriano@iescarlesvallbona.cat>
2023-02-15 20:24:19 +0100

    Componentes

--- AgustinAdrianM13Restaurante/src/main/java/com/mycompany/agustinadrianm13restaurante/PrimaryController.java
+++ AgustinAdrianM13Restaurante/src/main/java/com/mycompany/agustinadrianm13restaurante/PrimaryController.java
@@ -1,12 +1,30 @@
 package com.mycompany.agustinadrianm13restaurante;
 
 import java.io.IOException;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
 public class PrimaryController {
 
     @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
+    }
+    
+    @FXML
+    private Button btnComandes;
+
+    @FXML
+    private Button btnReserves;
+
+    @FXML
+    void clickBtnComandes(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickBtnReserves(ActionEvent event) {
+
     }
 }
