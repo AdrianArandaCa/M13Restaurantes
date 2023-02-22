b519eebb4215bf8abe13fbf8ca3612cbce75462c
Agustin <agusti_cintassoriano@iescarlesvallbona.cat>
2023-02-22 20:44:16 +0100

    DBCONNECTOR

--- AgustinAdrianM13Restaurante/src/main/java/com/mycompany/agustinadrianm13restaurante/PrimaryController.java
+++ AgustinAdrianM13Restaurante/src/main/java/com/mycompany/agustinadrianm13restaurante/PrimaryController.java
@@ -1,5 +1,6 @@
 package com.mycompany.agustinadrianm13restaurante;
 
+import Controller.ReservaController;
 import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -7,6 +8,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -29,7 +31,21 @@ public class PrimaryController {
     }
 
     @FXML
-    void clickBtnReserves(ActionEvent event) throws IOException {
+    void clickBtnReserves(ActionEvent event)  {
+        
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("Reserva.fxml"));
+            Parent root = loader.load();
+            
+            ReservaController controllerReserva = loader.getController();
+            Scene scene = new Scene(root);
+            Stage stage = new Stage();
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.setScene(scene);
+            stage.showAndWait();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
         
     }
 }
--- AgustinAdrianM13Restaurante/src/main/java/module-info.java
+++ AgustinAdrianM13Restaurante/src/main/java/module-info.java
@@ -2,7 +2,11 @@ module com.mycompany.agustinadrianm13restaurante {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.base;
+    requires java.sql;
 
     opens com.mycompany.agustinadrianm13restaurante to javafx.fxml;
     exports com.mycompany.agustinadrianm13restaurante;
+    
+    opens Controller to javafx.fxml;
+    exports Controller;
 }
